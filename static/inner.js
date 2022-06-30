13d402203f44f763c7351d6ba1e011b58d6d642b
Arthur Edelstein <arthuredelstein@gmail.com>
2022-06-30 14:08:45 +0000

    simplify

--- static/inner.js
+++ static/inner.js
@@ -4,6 +4,7 @@ const kWebSocketAddress = "wss://torpat.ch/poolparty/websockets";
 const waitInterval = 0;
 const kMaxValue = 128;
 const stepMs = 120;
+const kMaxSlots = 256;
 
 // All sockets, dead or alive.
 const sockets = new Set();
@@ -53,13 +54,13 @@ const probe = async (max) => {
 };
 
 const amISender = async () => {
-  const found = await consumeSockets(300);
+  const found = await consumeSockets(kMaxSlots);
   return found > 128;
 };
 
 const sendIntegers = async (n) => {
   const integerList = [];
-  await consumeSockets(300);
+  await consumeSockets(kMaxSlots);
   const startTime = performance.now();
   let lastInteger = 0;
   for (let i = 0; i < n; ++i) {
@@ -77,7 +78,7 @@ const sendIntegers = async (n) => {
     console.log("sent:", integer - 1, Date.now());
     await sleepMs(Math.max(0, remainingTime));
   }
-  await consumeSockets(300);
+  await consumeSockets(kMaxSlots);
   return integerList;
 };
 
@@ -108,80 +109,58 @@ const receiveIntegers = async (n) => {
 // Display elements
 const logDiv = document.getElementById("log");
 
-// Update the display elements
-const update = ({ consumedCount, probeFound, role, sent, received, time }) => {
-  let message = "";
-  if (consumedCount !== undefined) {
-    message += "last consumed: " + consumedCount;
-  }
-  if (probeFound !== undefined) {
-    message += "probe found: " + probeFound;
-  }
-  if (role !== undefined) {
-    message += "role: " + role;
-  }
-  if (sent !== undefined) {
-    message += "sent: " + sent;
-  }
-  if (received !== undefined) {
-    message += "received: " + received;
-  }
-  message += ", holding: " + sockets.size;
-  if (time !== undefined) {
-    message += ", time: " + time;
-  }
-  logDiv.innerText += message + "\n";
-};
-
 // Input elements
-const consumeOneButton = document.getElementById("consumeOne");
-const consumeAllButton = document.getElementById("consumeAll");
-const releaseOneButton = document.getElementById("releaseOne");
-const releaseAllButton = document.getElementById("releaseAll");
-const probeButton = document.getElementById("probe");
-const roleButton = document.getElementById("role");
-const sendIntegersButton = document.getElementById("sendIntegers");
-const receiveIntegersButton = document.getElementById("receiveIntegers");
+const commandButtonsDiv = document.getElementById("commandButtons");
 
 // Wire up input elements:
 
-const bindCommandToButton = (button, command, resultName) => {
+const createButtonForCommand = (commandName, commandFunction) => {
+  let button = document.createElement("button");
+  button.id = "button-" + commandName.replace(" ", "-");;
+  button.innerText = commandName;
   button.addEventListener("click", async (_e) => {
     const t1 = performance.now();
-    const result = await command();
+    const result = await commandFunction();
     const t2 = performance.now();
     const resultObject = { time: t2 - t1 };
-    resultObject[resultName] = result;
-    update(resultObject);
+    logDiv.innerText += `${commandName}: ${result}, time: ${t2-t1}, holding: ${sockets.size}\n`;
   });
+  commandButtons.appendChild(button);
 };
 
-bindCommandToButton(
-  consumeOneButton,
-  () => consumeSockets(1),
-  "consumedCount");
-bindCommandToButton(
-  consumeAllButton,
-  () => consumeSockets(300),
-  "consumedCount");
-bindCommandToButton(
-  releaseOneButton,
-  async () => { return -await releaseSockets(1); },
-  "consumedCount");
-bindCommandToButton(
-  releaseAllButton,
-  async () => { return -await releaseSockets(300); },
-  "consumedCount");
-bindCommandToButton(probeButton, () => probe(300), "probeFound");
-bindCommandToButton(
-  roleButton,
-  async () => { return await amISender() ? "sender" : "receiver"; },
-  "role");
-bindCommandToButton(
-  sendIntegersButton,
-  () => sendIntegers(5),
-  "sent");
-bindCommandToButton(
-  receiveIntegersButton,
-  () => receiveIntegers(5),
-  "received");
+createButtonForCommand("consume 1", () => consumeSockets(1));
+createButtonForCommand("consume all", () => consumeSockets(kMaxSlots));
+createButtonForCommand("release 1", () => releaseSockets(1));
+createButtonForCommand("release all", () => releaseSockets(kMaxSlots));
+createButtonForCommand("probe", () => probe(kMaxSlots));
+createButtonForCommand("is sender", amISender);
+createButtonForCommand("send", () => sendIntegers(5));
+createButtonForCommand("receive", () => receiveIntegers(5));
+
+const t0 = performance.now();
+let sender = await amISender();
+if (sender) {
+  await consumeSockets(kMaxSlots);
+  while (true) {
+    await releaseSockets(1);
+    const consumed = await consumeSockets(1);
+    if (consumed === 0) {
+      break;
+    }
+  }
+  await sleepMs(100);
+  await consumeSockets(1);
+  const t1 = performance.now();
+  console.log("prep: ", t1);
+  const resultList = await sendIntegers(5);
+  const t2 = performance.now();
+  await releaseSockets(kMaxSlots);
+  logDiv.innerText += "send: " + resultList + ", time, ms: " + (t2 - t1) + "\n";
+} else {
+  await consumeSockets(1);
+  await sleepMs(100);
+  await releaseSockets(kMaxSlots);
+  const t1 = performance.now();
+  const resultList = await receiveIntegers(5);
+  logDiv.innerText += "received: " + resultList + ", prep time, ms:" + (t1 - t0) + "\n";
+}
