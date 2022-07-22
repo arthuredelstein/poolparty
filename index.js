b2b87207e21a67dff678fa2975362a35c46715ed
Arthur Edelstein <arthuredelstein@gmail.com>
2022-07-22 17:00:01 +0000

    sse server code

--- index.js
+++ index.js
@@ -4,8 +4,38 @@ const { WebSocketServer } = require('ws');
 var app = express();
 
 app.get('/', function(req, res, next){
-  res.send("hello\n");
+  console.log("hello");
+  res.send("hello, welcome to events\n");
 });
+
+app.get('/yo', function(req, res, next){
+  console.log("wassup hi");
+  res.send("wassup hi\n");
+});
+
+app.get('/source', async function(req, res) {
+  console.log('Got /source');
+//  res.send("hi from /source");
+  res.set({
+    'Cache-Control': 'no-cache',
+    'Content-Type': 'text/event-stream',
+    'Connection': 'keep-alive'
+  });
+  res.flushHeaders();
+
+  // Tell the client to retry every 10 seconds if connectivity is lost
+  res.write('retry: 10000\n\n');
+  let count = 0;
+/*
+  while (true) {
+    await new Promise(resolve => setTimeout(resolve, 1000));
+
+    console.log('Emit', ++count);
+    // Emit an SSE that contains the current 'count' as a string
+    res.write(`data: ${count}\n\n`);
+  }*/
+});
+
 
 app.listen(3501);
 
